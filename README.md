142eeb361c2af5b86835123935349393c6b91883
nexxai <nexxai@gmail.com>
2024-04-17 16:23:17 -0600

    Update after fixing npm packages

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Realitype
 
 This is a proof-of-concept Laravel app / React component that provides some
-semblance of autocorrect.  It is dependent on a working copy of [Silvio's
+semblance of autocorrect. It is dependent on a working copy of [Silvio's
 LanguageTool](https://github.com/silvio/docker-languagetool) as it uses that
 container for the actual spell checking.
+
+## Interesting stuff
+
+The interesting code can be found in `resources/js/Components/TextArea.jsx` and
+`resources/js/Pages/Typing.jsx`.
 
 ## Installation
 
@@ -20,3 +25,9 @@ to update the `LANGUAGETOOL_BASE_URL` environment variable.
 1. `bun run dev`
 2. `php artisan serve`
 3. Open a browser and navigate to [the web interface demo](http://localhost:8000)
+
+## Digging into the code
+
+If you are interested in the API calls to LanguageTool, they can be found at
+`/app/Http/Controllers/WordCheckController.php` The code is most definitely
+pretty but it works (most of the time).
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('typing');
 });
 
 Route::post('/word/validate', [\App\Http\Controllers\WordCheckController::class, 'validate'])->name('validate');
 
 Route::get('/typing', function () {
     return Inertia::render('Typing');
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->name('typing');
 
 require __DIR__.'/auth.php';
